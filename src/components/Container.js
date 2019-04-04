4536518d29c1add09fa85effba5d4a09196cb298
Thomas Kim <thomas.keunback.kim@gmail.com>
2019-04-04 13:05:44 -0400

    Second commit

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-let url = 'https://weather-ydn-yql.media.yahoo.com/forecastrss?format=json';
+const sampleKey = '7ae74236b4e2e58155be1c0b93204d31';
+const location = [
+	{ 
+		name: 'Toronto',
+		lat: 43.6532,
+		long: 79.3832
+	}
+];
+
+const url = 'https://api.darksky.net/forecast/'+ {sampleKey} + '/' + location.lat +',' + location.long
 
 fetch(url, {
 	mode: 'no-cors',
-}).then(function(response) {
+	headers: {
+                "Content-Type": "application/json"
+            }
+})
+.then(function(response) {
       return response.json(); // This method is part of the fetch API and returns another promise
   })
   .then(function(data) {
